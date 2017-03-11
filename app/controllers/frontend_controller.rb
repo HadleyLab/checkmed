f64047ed4adbd37ae0d4e65c8f1ce6dfc9e04a72
Ivan Dymkov <mstrdymio@gmail.com>
2017-03-12 00:24:43 +0700

    Add devise controllers and views for users and configure it a little

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -4,17 +4,10 @@ class FrontendController < ApplicationController
 
   def seo
     @seo_carrier ||= nil
-    @seo ||= Seo::Basic.new @seo_carrier, false, setting_value('site_name')
+    @seo ||= Seo::Basic.new @seo_carrier,
+                            false,
+                            Rails.application.config.app_humanized_name
   end
 
-  def setting_value(setting_name)
-    case setting_name
-    when :app_humanized_name
-      Rails.application.config.app_humanized_name
-    else
-      nil
-    end
-  end
-
-  helper_method :setting_value, :seo
+  helper_method :seo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  devise_for :users
+  devise_for :users, controllers: {
+                       passwords: 'users/passwords',
+                       registrations: 'users/registrations',
+                       sessions: 'users/sessions' }
 
   # You can have the root of your site routed with "root"
   root 'home#index'
