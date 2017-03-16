73ffd9eb41874f884ff373340f8852d1d09585d1
Ivan Dymkov <mstrdymio@gmail.com>
2017-03-16 20:07:41 +0700

    Add layout builded with mdl and pair of pages in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require material
+// do not require turbolinks because of material js
+// do not require_tree .
 
 function add_checklist_item_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
   $(link).parent().before(content.replace(regexp, new_id));
 }
+
+$(document).ready(function() {
+  var flmd;
+  if ((flmd = $("#flash-message-deliverer")).length) {
+    var msg = flmd.data('error') || flmd.data('alert') || flmd.data('notice');
+    var notification = document.querySelector('.mdl-js-snackbar');
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: msg
+      }
+    );
+    // TODO can't take MaterialSnackbar
+  }
+});
