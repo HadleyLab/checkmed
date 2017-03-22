7090e8900ec4a43988f507261923ca60f6db078c
Ilya Beda <ir4y.ix@gmail.com>
2017-03-22 16:21:44 +0700

    Add deploy stage, move deploy scenario to the .gitlab-ci.yml

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose -f docker-compose.production.yml pull
-docker-compose -f docker-compose.production.yml run --rm web rake db:migrate
-docker-compose -f docker-compose.production.yml up -d
