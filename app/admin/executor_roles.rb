e05c0b1b5ea8667944f0912723e53aae1d4c5d17
Ivan Dymkov <mstrdymio@gmail.com>
2017-03-27 17:31:13 +0700

    Tune up the executor roles admin

--- app/admin/executor_roles.rb
+++ app/admin/executor_roles.rb
@@ -1,5 +1,17 @@
 ActiveAdmin.register ExecutorRole do
   permit_params :name, :prior
 
+  actions :all, except: [:show]
+
   config.sort_order = 'prior_asc'
+
+  filter :name
+  filter :prior
+
+  index download_links: false do
+    selectable_column
+    column :name
+    column :prior
+    actions
+  end
 end
