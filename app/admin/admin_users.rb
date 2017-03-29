164b79a732fe7c47bffb139bf70964ae5d35b8b6
Ivan Dymkov <mstrdymio@gmail.com>
2017-03-29 20:29:04 +0700

    Improve admin users admin section

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -5,9 +5,10 @@ ActiveAdmin.register AdminUser do
 
   index do
     selectable_column
-    id_column
+    column :name do |admin_user|
+      link_to admin_user.name, admin_admin_user_path(admin_user)
+    end
     column :email
-    column :name
     # column :current_sign_in_at
     # column :sign_in_count
     column :created_at
@@ -19,6 +20,16 @@ ActiveAdmin.register AdminUser do
   # filter :current_sign_in_at
   # filter :sign_in_count
   filter :created_at
+
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :created_at
+      row :updated_at
+      row 'Remebered at', :remember_created_at
+    end
+  end
 
   form do |f|
     f.inputs "Admin Details" do
