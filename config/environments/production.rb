518bc733c1cd622612118f48632c51f398b38084
Ilya Beda <ir4y.ix@gmail.com>
2017-03-31 11:03:48 +0700

    Setup AWS SES from mail delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => ENV["SMTP_SERVER"],
     :port                 => ENV["SMTP_PORT"].to_i,
-    :domain               => ENV["SMTP_DOMAIN"]
+    :domain               => ENV["SMTP_DOMAIN"],
+    :user_name            => ENV["SMTP_USERNAME"],
+    :password             => ENV["SMTP_PASSWORD"],
+    :authentication       => :login,
+    :enable_starttls_auto => true,
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
