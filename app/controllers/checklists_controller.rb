932e7ddfc0aeb8d54c27337bf9909c3b9abfc4fb
Vadim Laletin <rusmiligamer@gmail.com>
2017-09-08 15:55:09 +0700

    UI changes
    Rename "question" attribute to "comment".
    Replace 'New question' button with 'Add comment/picture' button.
    Display the only one questions group "Symptom" by default.

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -95,11 +95,7 @@ class ChecklistsController < FrontendController
     @seo_carrier = OpenStruct.new title: "New checklist"
 
     # Add default groups
-    @checklist.groups.build name: 'Symptoms questions',       prior: 0
-    @checklist.groups.build name: 'Physical exams questions', prior: 1
-    @checklist.groups.build name: 'Labs questions',           prior: 2
-    @checklist.groups.build name: 'Procedures questions',     prior: 3
-    @checklist.groups.build name: 'Medications questions',    prior: 4
+    @checklist.groups.build name: 'Symptoms',       prior: 0
 
     respond_to do |format|
       format.html
