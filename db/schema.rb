20852391bfe350c9d5eddeca1125aafe1631ccd6
Pavel Rozhkov <prozskov@gmail.com>
2018-04-16 15:25:57 +0700

    Changes in schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416021632) do
+ActiveRecord::Schema.define(version: 20180416065627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20180416021632) do
     t.datetime "updated_at",                        null: false
     t.integer  "speciality_id"
     t.integer  "checklist_type_id"
+    t.boolean  "published",         default: false
   end
 
   add_index "checklists", ["checklist_type_id"], name: "index_checklists_on_checklist_type_id", using: :btree
