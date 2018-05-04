f5a54596ce477d0edf322ff1d83432cea966e4f9
Pavel Rozhkov <prozskov@gmail.com>
2018-05-04 16:20:51 +0700

    Remove css for tabs and add condition for mobile devices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,4 +63,12 @@ module ApplicationHelper
     outer_links.compact
   end
 
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
 end
